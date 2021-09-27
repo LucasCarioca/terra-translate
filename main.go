04feee1ce802a5a40c235f894a72b162da301e74
Lucas Desouza <lucasfds2777@gmail.com>
2021-09-27 00:06:33 -0500

    adding basic summary reading functionality

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	terra_translate "github.com/LucasCarioca/terra-translate/pkg/terra-translate"
 	"github.com/LucasCarioca/terra-translate/pkg/utilities"
 	"os"
 )
@@ -13,5 +14,16 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Printf(input)
+	//fmt.Printf(input)
+	summary, err := terra_translate.GetSummary(input)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Printf("operation: %s\n", summary.Operation)
+	fmt.Printf("changes: %d\n", summary.Change)
+	fmt.Printf("add: %d\n", summary.Add)
+	fmt.Printf("destroy: %d\n", summary.Remove)
 }
--- pkg/terra-translate/terra-translate.go
+++ pkg/terra-translate/terra-translate.go
@@ -1,6 +1,28 @@
 package terra_translate
 
+import (
+	"encoding/json"
+	"strings"
+)
 
-type Example struct {
+type Changes struct {
+	Add int				`json:"add"`
+	Change int			`json:"change"`
+	Remove int			`json:"remove"`
+	Operation string 	`json:"operation"`
+}
 
+type rawChanges struct {
+	Changes Changes `json:"changes"`
+}
+
+func GetSummary(data string) (Changes, error) {
+	sections := strings.SplitAfter(data, "} ")
+	summaryData := sections[len(sections)-1]
+	summary := rawChanges{}
+	err := json.Unmarshal([]byte(summaryData), &summary)
+	if err != nil {
+		return Changes{}, err
+	}
+	return summary.Changes, nil
 }
